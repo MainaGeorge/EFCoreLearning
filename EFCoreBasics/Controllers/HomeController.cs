05a390a636a078106cee3ed42b42d3ea2a0fa9d1
MainaGeorge <nuriszka@gmail.com>
2020-09-12 09:47:32 +0200

    setting up the project

---
+++ EFCoreBasics/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreBasics.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+    }
+}
